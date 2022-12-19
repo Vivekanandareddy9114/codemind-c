4276582ab4919c26556a86b28e819887531e963d
Vivekanandareddy9114 <119731762+Vivekanandareddy9114@users.noreply.github.com>
2022-12-19 20:35:42 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n==1)     printf("monday");     else if(n==2)     printf("tuesday");     else if(n==3)     printf("Wednesday");     else if(n==4)     printf("Thursday");     else if(n==5)     printf("Firday");     else if(n==6)     printf("Saturday");     else     printf("invalid input"); }

---
+++ Day_name__.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n==1)
+    printf("monday");
+    else if(n==2)
+    printf("tuesday");
+    else if(n==3)
+    printf("Wednesday");
+    else if(n==4)
+    printf("Thursday");
+    else if(n==5)
+    printf("Firday");
+    else if(n==6)
+    printf("Saturday");
+    else
+    printf("invalid input");
+}
